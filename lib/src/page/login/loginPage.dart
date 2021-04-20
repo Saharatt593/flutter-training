df7d6a611ec8c3127b381a8601fb5ef379eb069f
SAHARATT593 <saharatt.s@cdg.co.th>
2021-04-20 13:28:36 +0700

    bottom center

--- lib/src/page/login/loginPage.dart
+++ lib/src/page/login/loginPage.dart
@@ -18,19 +18,72 @@ class LoginPage extends StatelessWidget {
                     end: Alignment.bottomCenter,
                     stops: [0.0, 0.5])),
           ),
-          Column(
-
-              crossAxisAlignment: CrossAxisAlignment.center,
+          Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            SizedBox(
+              height: 48,
+            ),
+            Image.asset(
+              "assets/images/cdgs_logo.png",
+              width: 200,
+            ),
+            Stack(
+              alignment: Alignment.bottomCenter,
               children: [
-                SizedBox(height: 48,),
-                Image.asset("assets/images/cdgs_logo.png",
-                width: 200,),
-                Text("Header"),
-                Text("Login"),
-                Text("Login button"),
-                Text("forget button"),
-                Text("sso"),
-              ])
+                Card(
+                  margin: const EdgeInsets.only(
+                    top: 22,
+                    bottom: 24,
+                    right: 22,
+                    left: 22,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 22,
+                      bottom: 62,
+                      right: 22,
+                      left: 22,
+                    ),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              hintText: "login",
+                              labelText: "username",
+                              icon: Icon(Icons.person),
+                              border: InputBorder.none),
+                        ),
+                        Divider(
+                          height: 22,
+                          indent: 22,
+                          endIndent: 22,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "password",
+                              icon: Icon(Icons.lock),
+                              border: InputBorder.none),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 280,
+                  height: 52,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Login",
+                        style:
+                            TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                      )),
+                )
+              ],
+            ),
+            Text("Login button"),
+            Text("forget button"),
+            Text("sso"),
+          ])
         ],
       ),
     );
