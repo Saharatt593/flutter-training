6898ba20968ffb3c4d6fbe0b036f8715b656846d
SAHARATT593 <saharatt.s@cdg.co.th>
2021-04-21 09:53:34 +0700

    update api

---
+++ lib/src/constants/api.dart
@@ -0,0 +1,5 @@
+class API {
+  static const String BASE_URL = 'https://f64c6ea9af8c.jp.ngrok.io';
+  static const String IMAGE_URL = '$BASE_URL/images';
+  static const String PRODUCT = '/products';
+}
