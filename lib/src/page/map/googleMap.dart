daa4154fd4529ef38b13baadd18904d9b260c196
SAHARATT593 <saharatt.s@cdg.co.th>
2021-04-22 09:53:06 +0700

    pin in map

--- lib/src/page/map/googleMap.dart
+++ lib/src/page/map/googleMap.dart
@@ -1,5 +1,9 @@
 import 'dart:async';
+import 'dart:ui';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app2/src/constants/asset.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapPage extends StatefulWidget {
@@ -11,7 +15,7 @@ class GoogleMapPageState extends State<GoogleMapPage> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _initMap = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(13.6972552, 100.5131413),
     zoom: 14.4746,
   );
 
@@ -38,7 +42,7 @@ class GoogleMapPageState extends State<GoogleMapPage> {
           GoogleMap(
             trafficEnabled: true,
             markers: _marker,
-            mapType: MapType.none,
+            mapType: MapType.normal,
             initialCameraPosition: _initMap,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
@@ -47,30 +51,84 @@ class GoogleMapPageState extends State<GoogleMapPage> {
           Positioned(
               left: 8,
               child: ElevatedButton.icon(
-                onPressed: _goToTheLake,
+                onPressed: _pinMarker,
                 icon: Icon(Icons.pin_drop),
                 label: Text("Pin Marker"),
               ))
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+      // floatingActionButton: FloatingActionButton.extended(
+      //   onPressed: _pinMarker,
+      //   label: Text('To the lake!'),
+      //   icon: Icon(Icons.directions_boat),
+      // ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _pinMarker() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_codeMobile));
-    dummyData.forEach((element) {
-      final marker = Marker(
-        markerId: MarkerId(element.toString()),
-        position: element,
-      );
-      _marker.add(marker);
-      setState(() {});
-    });
+     for(var latLng in dummyData){
+       await _addMarker(latLng , title:"xx",snippet: "yy",isShowInfo: true);
+    }
+
+    setState(() {});
+
+    // dummyData.forEach((element) {
+    //   final marker = Marker(
+    //     markerId: MarkerId(element.toString()),
+    //     position: element,
+    //   );
+    //   _marker.add(marker);
+    //   setState(() {});
+    // });
+  }
+  Future<Uint8List> getBytesFromAsset(String path, double height) async {
+    final byteData = await rootBundle.load(path);
+    final codec = await instantiateImageCodec(
+      byteData.buffer.asUint8List(),
+      targetHeight: height.toInt(),
+    );
+    final frameInfo = await codec.getNextFrame();
+    final image = await frameInfo.image.toByteData(format: ImageByteFormat.png);
+    return image.buffer.asUint8List();
+  }
+
+  Future<void> _addMarker(
+      LatLng position, {
+        String title = 'none',
+        String snippet = 'none',
+        String pinAsset = Asset.pinBikerImage,
+        bool isShowInfo = false,
+      }) async {
+    final byteData = await getBytesFromAsset(
+      pinAsset,
+      MediaQuery.of(context).size.height * 0.2,
+    );
+
+    final bitmapDescriptor = BitmapDescriptor.fromBytes(byteData);
+
+    final marker = Marker(
+      // important. unique id
+      markerId: MarkerId(position.toString()),
+      position: position,
+      infoWindow: isShowInfo
+          ? InfoWindow(
+        title: title,
+        snippet: snippet,
+        onTap: () {
+          // _launchMaps(
+          //   position.latitude,
+          //   position.longitude,
+          // );
+        },
+      )
+          : null,
+      icon: bitmapDescriptor,
+      onTap: () {
+        print('lat: ${position.latitude}, lng: ${position.longitude}');
+      },
+    );
+    _marker.add(marker);
   }
 }
