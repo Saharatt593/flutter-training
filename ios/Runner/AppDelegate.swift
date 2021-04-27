7c2ae72a593d251ac0d52ff3fcd05d064d92b102
SAHARATT593 <saharatt.s@cdg.co.th>
2021-04-27 15:20:30 +0700

    delete api_KEY

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDits4D2kL76TcVqrunCTjz3d9yprvWob8")
+    GMSServices.provideAPIKey("{API_KEY}")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
